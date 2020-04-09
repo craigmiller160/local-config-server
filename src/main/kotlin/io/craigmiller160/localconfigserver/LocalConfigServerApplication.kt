41944bc89280b3c94e0575aca0a2ef7741002a2f
Craig Miller <craigmiller160@gmail.com>
2020-04-09 15:40:58 -0400

    Added some config classes

--- src/main/kotlin/io/craigmiller160/localconfigserver/LocalConfigServerApplication.kt
+++ src/main/kotlin/io/craigmiller160/localconfigserver/LocalConfigServerApplication.kt
@@ -2,10 +2,8 @@ package io.craigmiller160.localconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
-@EnableConfigServer
 class LocalConfigServerApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/io/craigmiller160/localconfigserver/config/WebSecurityConfig.kt
@@ -0,0 +1,8 @@
+package io.craigmiller160.localconfigserver.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class WebSecurityConfig {
+
+}
