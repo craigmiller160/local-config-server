02d1580a625dc4193e1d3637517415f7848a5d92
Craig Miller <craigmiller160@gmail.com>
2020-05-30 18:34:18 -0400

    Got a deployment script built to the point where it builds the container

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=$(cat pom.xml | grep version | head -n3 | tail -n1)
+version=$(echo $version | sed 's/<version>//g' | sed 's/<\/version>//g')
+
+#if [ $# -ne 1 ]; then
+#  echo "Must provide version number"
+#  exit 1
+#fi
+
+NAME=local-config-server
+VERSION=$version
+
+mvn clean package
+eval $(minikube docker-env)
+docker build -t $NAME:$VERSION .
